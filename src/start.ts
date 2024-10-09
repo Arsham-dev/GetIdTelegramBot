a3a42ddb74b04f7108ca4b6eb15899b9389025b7
Arsham <arsham.ramezani@gmail.com>
2024-10-09 01:20:11 -0700

    fix: start message bug fixed

--- src/start.ts
+++ src/start.ts
@@ -4,7 +4,20 @@ import { Update } from 'telegraf/typings/core/types/typegram'
 const handleStart = (Bot:Telegraf<Context<Update>>) => {
   Bot.start(async (ctx) => {
     await ctx.reply('Hi , Welcome to get id bot')
-    await ctx.reply(`Your ID is: ${ctx.from?.id}\nYour Username is: ${ctx.from?.username}\nYour First Name is: ${ctx.from?.first_name}\nYour Last Name is: ${ctx.from?.last_name}`)
+    const message = []
+    if (ctx.from?.id) {
+      message.push(`Your ID: ${ctx.from.id}`)
+    }
+    if (ctx.from?.username) {
+      message.push(`Your Username: ${ctx.from.username}`)
+    }
+    if (ctx.from?.first_name) {
+      message.push(`Your First Name: ${ctx.from.first_name}`)
+    }
+    if (ctx.from?.last_name) {
+      message.push(`Your Last Name: ${ctx.from.last_name}`)
+    }
+    await ctx.reply(message.join('\n'))
   })
 }
 
