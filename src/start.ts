2bf28b152ed7ee4429bd1630a021f4f6384eaf93
Arsham <arsham.ramezani@gmail.com>
2026-04-01 14:25:44 -0700

    feat: chat group id added feat: copy mode added

--- src/start.ts
+++ src/start.ts
@@ -1,26 +1,58 @@
 import { Context, Telegraf } from 'telegraf'
-import { Update } from 'telegraf/typings/core/types/typegram'
+import { Chat, Update } from 'telegraf/typings/core/types/typegram'
 
-const handleStart = (Bot:Telegraf<Context<Update>>) => {
+const getUserMessage = (ctx: Context<Update>): string => {
+  const message = []
+  const chat = ctx.from
+
+  if (chat?.id) {
+    message.push(`Your ID: \`${chat.id}\``)
+  }
+  if (chat?.username) {
+    message.push(`Your Username: \`${chat.username}\``)
+  }
+  if (chat?.first_name) {
+    message.push(`Your First Name: \`${chat.first_name}\``)
+  }
+  if (chat?.last_name) {
+    message.push(`Your Last Name: \`${chat.last_name}\``)
+  }
+
+  return message.join('\n')
+}
+
+const getGroupMessage = (ctx: Context<Update>): string => {
+  const message = []
+  const chat = ctx.chat as Chat.GroupChat | Chat.SupergroupChat
+
+  if (chat?.id) {
+    message.push(`Group ID: \`${chat.id}\``)
+  }
+  if (chat?.title) {
+    message.push(`Group Title: \`${chat.title}\``)
+  }
+
+  return message.join('\n')
+}
+
+const handleStart = (Bot: Telegraf<Context<Update>>) => {
   Bot.start(async (ctx) => {
     await ctx.reply('Hi , Welcome to get id bot')
-    const message = []
-    const chat = ctx.from
+    let message = ''
 
-    if (chat?.id) {
-      message.push(`Your ID: ${chat.id}`)
-    }
-    if (chat?.username) {
-      message.push(`Your Username: ${chat.username}`)
-    }
-    if (chat?.first_name) {
-      message.push(`Your First Name: ${chat.first_name}`)
-    }
-    if (chat?.last_name) {
-      message.push(`Your Last Name: ${chat.last_name}`)
+    switch (ctx.chat.type) {
+      case 'private':
+        message = getUserMessage(ctx)
+        break
+      case 'group':
+      case 'supergroup':
+        message = getGroupMessage(ctx)
+        break
+      default:
+        message = ''
     }
 
-    await ctx.reply(message.join('\n'))
+    await ctx.reply(message)
   })
 }
 
